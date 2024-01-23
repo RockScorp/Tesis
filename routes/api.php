791bfbc8f11c86331a8ef801ab6604ea31195382
RockScorp <rockiiscorpio@gmail.com>
2024-01-23 12:15:26 -0500

    primer commit ;3

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TesisController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('tabla_1/get', [TesisController::class,'get_tabla_1']);
+Route::get('tabla_1/create', [TesisController::class,'create_tabla_1']);
+Route::get('tabla_1/update', [TesisController::class,'update_tabla_1']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
