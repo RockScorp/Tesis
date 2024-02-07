d187493e1e2cc89373061ba8b2b638c2043b8fde
RockScorp <rockiiscorpio@gmail.com>
2024-02-07 11:36:14 -0500

    integración_seeder_3

--- database/seeders/Table_5_6_Seeder.php
+++ database/seeders/Table_5_6_Seeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Table_5_6;
 use Illuminate\Database\Seeder;
 
 class Table_5_6_Seeder extends Seeder
@@ -13,6 +14,26 @@ class Table_5_6_Seeder extends Seeder
      */
     public function run()
     {
-        //
+        Table_5_6::create([
+            "campo_1"=>"servicio fijo example 1", //luz / agua ?
+            "campo_2"=>"descripción example 1",
+            "table_3_id"=>1
+        ]);
+        Table_5_6::create([
+            "campo_1"=>"servicio fijo example 2",
+            "campo_2"=>"descripción example 2",
+            "table_3_id"=>null
+        ]);
+
+        Table_5_6::create([
+            "campo_1"=>"servicio variable example 1", //luz / agua ?
+            "campo_2"=>"descripción example 1",
+            "table_3_id"=>2
+        ]);
+        Table_5_6::create([
+            "campo_1"=>"servicio variable example 2",
+            "campo_2"=>"descripción example 2",
+            "table_3_id"=>null
+        ]);
     }
 }
--- database/seeders/Table_7_Seeder.php
+++ database/seeders/Table_7_Seeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Table_7;
 use Illuminate\Database\Seeder;
 
 class Table_7_Seeder extends Seeder
@@ -13,6 +14,19 @@ class Table_7_Seeder extends Seeder
      */
     public function run()
     {
-        //
+        Table_7::create([
+            "campo_1"=> "materia prima example 1",
+            "campo_2"=> "descripcion example 1",
+            "campo_3"=> "codigo example 1",
+            "table_4_id"=> 3,
+            "campo_4"=> "25", //stock
+        ]);
+        Table_7::create([
+            "campo_1"=> "materia prima example 2",
+            "campo_2"=> "descripcion example 2",
+            "campo_3"=> "codigo example 2",
+            "table_4_id"=> 4,
+            "campo_4"=> "36", //stock
+        ]);
     }
 }
